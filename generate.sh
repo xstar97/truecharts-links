d41d9318c8c93f31d4a9c448b8205021e7b195e4
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2024-03-01 02:28:05 -0500

    update

--- generate.sh
+++ generate.sh
@@ -4,7 +4,7 @@
 git clone https://github.com/xstar97/go-littlelinks-generator.git .
 
 # Copy the files from the output directory to the parent directory
-cp -r output/* .
+cp -r output/* ./
 
 # Run the littlelink-generator with the specified parameters
 ./littlelink-generator --assets-path assets --config links.json
