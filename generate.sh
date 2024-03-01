22398dcd261ca1575e00a205a982851693631bfb
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2024-03-01 02:51:39 -0500

    update

--- generate.sh
+++ generate.sh
@@ -3,13 +3,12 @@
 # Clone the repository from GitHub into a temporary directory
 git clone https://github.com/xstar97/go-littlelinks-generator.git temp
 
-ls temp
 cp temp/output/littlelink-generator ./
 # Run the go-littlelinks-generator project
 ./littlelink-generator --asset-path "assets/" --config "links.json"
 
 # Copy the build directory to the root directory
-cp -r temp/build/* /build
+cp -r temp/build /build
 
 # Clean up temporary directory
 rm -rf temp
