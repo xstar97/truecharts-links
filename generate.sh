378f3918458299d55702a280fe5dd95b52a0cbfc
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2024-03-01 02:29:31 -0500

    update

--- generate.sh
+++ generate.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Clone the repository from GitHub into the current directory
-git clone https://github.com/xstar97/go-littlelinks-generator.git .
+# Clone the repository from GitHub into a temporary directory
+git clone --depth=1 https://github.com/xstar97/go-littlelinks-generator.git temp_dir
 
-# Copy the files from the output directory to the parent directory
-cp -r output/* ./
+# Copy the files from the output directory to the current directory
+cp -r temp_dir/output/* .
 
 # Run the littlelink-generator with the specified parameters
 ./littlelink-generator --assets-path assets --config links.json
+
+# Clean up temporary directory
+rm -rf temp_dir
