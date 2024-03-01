5ad823494608905f2807ff086f03b5310d76c159
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2024-03-01 02:39:26 -0500

    update

--- generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Clone the repository from GitHub into a temporary directory
-git clone --depth=1 https://github.com/xstar97/go-littlelinks-generator.git .
-
-# Run the littlelink-generator with the specified parameters
-go run cmd/main.go --assets-path assets/ --config links.json
