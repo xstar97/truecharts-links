dfc980476a063bd51466262eae8edc78ee58c7f5
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2024-03-01 02:14:31 -0500

    update

--- generate.sh
+++ generate.sh
@@ -3,11 +3,11 @@
 # Download the latest release zip file from GitHub
 wget -q https://github.com/xstar97/go-littlelinks-generator/releases/latest/download/go-littlelinks-generator.zip
 
-# Unzip the downloaded file into the root directory
-unzip -q go-littlelinks-generator.zip -d .
+# Unzip the downloaded file
+unzip -q go-littlelinks-generator.zip
 
 # Copy the output/littlelink-generator to the current directory
-cp /output/littlelink-generator .
+cp output/littlelink-generator .
 
 # Run the littlelink-generator with the specified parameters
-./littlelink-generator --assets-path assets/ --config links.json
+./littlelink-generator --assets-path assets --config links.json
