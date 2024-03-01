ef43b3bac3ebfa053591dda3d5918f47e9dfdaf6
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2024-03-01 02:33:29 -0500

    remove line

--- generate.sh
+++ generate.sh
@@ -5,6 +5,3 @@ git clone --depth=1 https://github.com/xstar97/go-littlelinks-generator.git .
 
 # Run the littlelink-generator with the specified parameters
 go run cmd/main.go --assets-path assets/ --config links.json
-
-# Clean up temporary directory
-rm -rf temp_dir
